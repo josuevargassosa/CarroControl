588ac1221b79403c17691097d8193f5e2d7442c8
Cesar Josue Vargas Sosa <34899519+josuevargassosa@users.noreply.github.com>
2026-01-15 00:35:02 -0500

    Initialize app dependencies with Hive and GetX

---
+++ lib/core/dependency_injection.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../controllers/car_controller.dart';
+import '../data/models/maintenance_model.dart';
+import '../data/models/maintenance_model.g.dart';
+import '../data/models/vehicle_model.dart';
+import '../data/models/vehicle_model.g.dart';
+
+Future<void> initApp() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(VehicleModelAdapter());
+  Hive.registerAdapter(MaintenanceModelAdapter());
+  await Hive.openBox('vehicles');
+  await Hive.openBox('maintenances');
+  Get.put(CarController());
+}
